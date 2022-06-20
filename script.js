edee4fb1cd9dcc8c982047067b5bbf824927778f
awidney <adamwidney@hotmail.ca>
2022-06-19 22:24:51 -0700

    Edit math/operate function(s); add event listeners
    Math functions round long decimal answers to prevent display overflow.
    
    Used HTML entities for operators. Operate function handles division by
    zero.
    
    Number event listeners populate display with entries.
    Respects a character display limit to prevent overflow from clicks.
    
    Operator event listeners store current operator, first/second operand.
    Calls the operate function when appropriate.
    
    Equals event listener checks if operate arguments are ready before
    calling the operate function.
    
    Clear event listener resets all conditions to default.
    
    The result, wiped, equalsToggled, and previousClick variables keep track
    of what has occurred throughout clicks and calculations. They allow for
    the correct code to be executed upon each click.

--- script.js
+++ script.js
@@ -1,53 +1,186 @@
-// Math functions
 function add(x, y) {
-    return x + y;            
+    return Math.round((x + y) * (10 ** 10)) / (10 ** 10);  
 }
 
 function subtract(x, y) {
-    return x - y;
+    return Math.round((x - y) * (10 ** 10)) / (10 ** 10);
 }
 
 function multiply(x, y) {
-    return x * y;
+    return Math.round((x * y) * (10 ** 10)) / (10 ** 10);
 }
 
 function divide(x, y) {
-    return x / y;
+    return Math.round((x / y) * (10 ** 10)) / (10 ** 10);
 }
 
-// Operate function calls appropriate math function
-// and updates display with result
-const result = document.querySelector('#result');
 
 function operate(operator, x, y) {
     if (operator == '+') {
-        result.textContent = add(x, y);
+        displayValue = add(x, y);
+        display.textContent = displayValue;
+        firstNumber = +displayValue;
+        currentOperator = '';
+        result = true;
+        wiped = false;
+        
     } else if (operator == '−') {
-        result.textContent = subtract(x, y);
+        displayValue = subtract(x, y);
+        display.textContent = displayValue;
+        firstNumber = +displayValue;
+        currentOperator = '';
+        result = true;
+        wiped = false;
+        
     } else if (operator == '×') {
-        result.textContent = multiply(x, y);
+        displayValue = multiply(x, y);
+        display.textContent = displayValue;
+        firstNumber = +displayValue;
+        currentOperator = '';
+        result = true;
+        wiped = false;
+        
     } else if (operator == '÷') {
-        result.textContent = divide(x, y);
+        if (y == 0) {
+            display.textContent = 'Say please.';
+            displayValue = '';
+            currentOperator = undefined;
+            firstNumber = undefined;
+            secondNumber = undefined;
+            previousClick = undefined;
+            result = false;
+            wiped = false;
+            equalsToggled = false;
+        } else {
+            displayValue = divide(x, y);
+            display.textContent = displayValue;
+            firstNumber = +displayValue;
+            currentOperator = '';
+            result = true;
+            wiped = false;
+        }
     }
 }
 
-// Adds click event for each button to update display with entries
-const buttons = document.querySelectorAll('.button');
+
+const numbers = document.querySelectorAll('.number');
+const operators = document.querySelectorAll('.button.operator');
 const display = document.querySelector('#display');
-
-buttons.forEach(button => {
-    button.addEventListener('click', function (e) {
-        display.textContent += e.target.textContent;
-    })
-});
-
-// Adds click event to equals button to parse display for operate arguments
-// and call the function
 const equals = document.querySelector('#equals');
+const clear = document.querySelector('#clear');
+const operatorSymbols = ['+', '−', '×', '÷'];
 
-equals.addEventListener('click', () => {
-    const operator = display.textContent.match(/\u002B|\u2212|\u00D7|\u00F7/).toString();
-    const x = +display.textContent.match(/[0-9]+/).toString();
-    const y = +display.textContent.match(/[0-9]+$/).toString();
-    operate(operator, x, y);
+
+let displayValue = '';
+let firstNumber;
+let currentOperator;
+let result = false;
+let wiped = false;
+let equalsToggled = false;
+let previousClick;
+
+
+numbers.forEach(number => {
+    number.addEventListener('click', function (e) {
+        
+        if ((display.textContent.length >= 15) && (previousClick.match(/[0-9]+/))) {
+            alert('Can\'t enter more than 15 digits.');
+            return;
+
+        } else if (result) {
+            if (!wiped) {
+                displayValue = '';
+                wiped = true;
+                displayValue = e.target.textContent;
+                display.textContent = displayValue;
+                previousClick = e.target.textContent;
+
+            } else {
+                displayValue += e.target.textContent;
+                display.textContent = displayValue;
+                previousClick = e.target.textContent;
+            }
+        
+        } else {
+            displayValue += e.target.textContent;
+            display.textContent = displayValue;
+            previousClick = e.target.textContent;
+
+        }
+    });
 });
+
+
+operators.forEach(operator => {
+    operator.addEventListener('click', function (e) {
+        
+            if (display.textContent == 'Say please.') {
+                return;
+
+            } else if ((currentOperator) && (!wiped)) {
+
+            if (operatorSymbols.includes(previousClick)) {
+                currentOperator = e.target.textContent;
+                previousClick = e.target.textContent;
+            
+            } else {
+                secondNumber = +displayValue;
+                operate(currentOperator, firstNumber, secondNumber);
+                currentOperator = e.target.textContent;
+                wiped = false;
+                previousClick = e.target.textContent;
+            }
+
+        
+        } else if (currentOperator) {
+            secondNumber = +displayValue;
+            operate(currentOperator, firstNumber, secondNumber);
+            currentOperator = e.target.textContent;
+            previousClick = e.target.textContent;
+            
+        } else {
+            currentOperator = e.target.textContent;
+            firstNumber = +displayValue;
+            displayValue = '';
+            previousClick = e.target.textContent;
+        }  
+    });
+});
+
+
+equals.addEventListener('click', function (e) {
+    if (previousClick == '=') {
+        return;
+    } else if (firstNumber == undefined) {
+        return;
+    } else if ((!previousClick.match(/[0-9]+/))) {
+        return;
+    } else if (!currentOperator) {
+        return;
+    } else {
+        secondNumber = +displayValue;
+        operate(currentOperator, firstNumber, secondNumber);
+        if (display.textContent == 'Say please.') {
+            return;
+        } else {
+            equalsToggled = true;
+            previousClick = e.target.textContent;
+        }
+    }
+});
+
+
+clear.addEventListener('click', () => {
+    display.textContent = '0';
+    displayValue = '';
+    currentOperator = undefined;
+    firstNumber = undefined;
+    secondNumber = undefined;
+    previousClick = undefined;
+    result = false;
+    wiped = false;
+    equalsToggled = false;
+});
+
+
+// Bug: numbers can overflow screen if too large
